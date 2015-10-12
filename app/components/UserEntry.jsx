8b47625d9d60d1bfbe4906d4e80ea79826877c22
Rob Warner <rwarner@grailbox.com>
2015-10-11 22:08:39 -0400

    Implement UserEntry as a form

--- app/components/UserEntry.jsx
+++ app/components/UserEntry.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 export default class UserEntry extends React.Component {
   render() {
     return (
-      <h1>User Entry</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="teen">Teen words</label>
+          <input type="text" className="form-control" id="teen" placeholder="What your teen said" onChange={this.textChanged} />
+          <p className="help-block pull-right">{this.state.characterCount}</p>
+        </div>
+        <button type="submit" className="btn btn-default">Translate</button>
+      </form>
     );
+  }
+  state = {
+    characterCount: 0
+  }
+  textChanged = (e) => {
+    this.setState({
+      characterCount: e.target.value.length
+    });
   }
 };
