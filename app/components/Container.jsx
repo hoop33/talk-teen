09e3191d47893ed8cb6c3d3367cfe147359a56f5
Rob Warner <rwarner@grailbox.com>
2015-10-11 22:09:02 -0400

    Pass title and subtitle to Title

--- app/components/Container.jsx
+++ app/components/Container.jsx
@@ -6,21 +6,11 @@ export default class Container extends React.Component {
   render() {
     return (
       <div className="container">
-        <Title />
-        <div className="row">
-          <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <UserEntry />
-          </div>
-          <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <UserEntry />
-          </div>
-          <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <UserEntry />
-          </div>
-          <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <UserEntry />
-          </div>
-        </div>
+        <Title
+          title="Talk Teen"
+          subtitle="Communicate with the incomprehensible"
+        />
+        <UserEntry />
       </div>
     );
   }
--- app/components/Title.jsx
+++ app/components/Title.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 export default class Title extends React.Component {
+  static propTypes = {
+    title: React.PropTypes.string.isRequired,
+    subtitle: React.PropTypes.string
+  }
   render() {
     return (
       <div className="jumbotron">
-        <h1>Talk Teen</h1>
-        <p>Communicate with the incomprehensible</p>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.subtitle}</p>
       </div>
     );
   }
